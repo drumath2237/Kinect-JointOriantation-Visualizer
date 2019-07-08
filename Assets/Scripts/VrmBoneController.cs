dbcae6de31eb510b844f2bf53116ba721ae862ba
drumath <2237k.drumath@gmail.com>
2019-07-09 03:12:12 +0900

    肩とひじの回転を適用

---
+++ Assets/Scripts/VrmBoneController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VrmBoneController : MonoBehaviour
+{
+    [SerializeField] GameObject LeftBone;
+    [SerializeField] GameObject LeftElbow;
+
+    Animator _animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _animator = GetComponent<Animator>();
+
+
+        LeftBone = GameObject.Find("ShoulderRight");
+        LeftElbow = GameObject.Find("ElbowRight");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _animator.GetBoneTransform(HumanBodyBones.LeftLowerArm).rotation = LeftElbow.transform.rotation;
+        _animator.GetBoneTransform(HumanBodyBones.LeftShoulder).rotation = LeftBone.transform.rotation;
+    }
+}
