aa0e6f36fcdd8a91a4effd1aece4c1adbdb82656
drumath <2237k.drumath@gmail.com>
2019-07-10 14:14:08 +0900

    両肩両腕腰のJoint⇒ボーンを設定

--- Assets/Scripts/VrmBoneController.cs
+++ Assets/Scripts/VrmBoneController.cs
@@ -4,8 +4,22 @@ using UnityEngine;
 
 public class VrmBoneController : MonoBehaviour
 {
-    [SerializeField] GameObject LeftBone;
-    [SerializeField] GameObject LeftElbow;
+    GameObject Head;
+    GameObject Neck;
+    GameObject Shoulder;
+
+    GameObject LeftShoulder, RightShoulder;
+    GameObject LeftElbow, RightElbow;
+    GameObject LeftWrist, RightWrist;
+
+    GameObject Chest;
+    GameObject Spine;
+    GameObject Hip;
+
+    GameObject LeftKnee, RightKnee;
+    GameObject LeftAnkle, RightAnkle;
+    GameObject LeftFoot, RightFoot;
+
 
     Animator _animator;
 
@@ -13,17 +27,31 @@ public class VrmBoneController : MonoBehaviour
     void Start()
     {
         _animator = GetComponent<Animator>();
-
-
-        LeftBone = GameObject.Find("ShoulderRight");
-        LeftElbow = GameObject.Find("ElbowRight");
-
     }
 
     // Update is called once per frame
     void Update()
     {
+        if(LeftShoulder==null)
+            CheckObjects();
+
         _animator.GetBoneTransform(HumanBodyBones.LeftLowerArm).rotation = LeftElbow.transform.rotation;
-        _animator.GetBoneTransform(HumanBodyBones.LeftShoulder).rotation = LeftBone.transform.rotation;
+        _animator.GetBoneTransform(HumanBodyBones.LeftShoulder).rotation = LeftShoulder.transform.rotation;
+
+        _animator.GetBoneTransform(HumanBodyBones.RightShoulder).rotation = RightShoulder.transform.rotation;
+        _animator.GetBoneTransform(HumanBodyBones.RightLowerArm).rotation = RightElbow.transform.rotation;
+
+        _animator.GetBoneTransform(HumanBodyBones.Spine).rotation = Spine.transform.rotation;
+    }
+
+    void CheckObjects()
+    {
+        LeftShoulder = GameObject.Find("ShoulderRight");
+        LeftElbow = GameObject.Find("ElbowRight");
+
+        RightShoulder = GameObject.Find("ShoulderLeft");
+        RightElbow = GameObject.Find("ElbowLeft");
+
+        Spine = GameObject.Find("SpineBase");
     }
 }
